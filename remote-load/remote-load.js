cb364aa4ec6ad0f8665e632efbded22cdaf5d4f4
lake-straly <128569602+lake-straly@users.noreply.github.com>
2023-09-14 10:40:10 -0700

    Delete remote-load directory

--- remote-load/remote-load.js
+++
@@ -1,9 +0,0 @@
-javascript:(function() {
-    var rawFileUrl = 'https://raw.githubusercontent.com/lake-straly/g5-bugfixes-and-improvements/main/package/remote-loader.js';
-    fetch(rawFileUrl)
-        .then(response => response.text())
-        .then(code => {
-            eval(code);
-        })
-        .catch(error => console.error('Error fetching code:', error));
-})();
