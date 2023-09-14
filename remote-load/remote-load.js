4b117b6b48807bbcdf19820b0e8c5b0ed69096d5
lake-straly <128569602+lake-straly@users.noreply.github.com>
2023-09-14 09:27:47 -0700

    Update remote-load.js

--- remote-load/remote-load.js
+++ remote-load/remote-load.js
@@ -1,1 +1,9 @@
-// Some remote load content here.
+javascript:(function() {
+    var rawFileUrl = 'https://raw.githubusercontent.com/lake-straly/g5-bugfixes-and-improvements/main/package/remote-loader.js';
+    fetch(rawFileUrl)
+        .then(response => response.text())
+        .then(code => {
+            eval(code);
+        })
+        .catch(error => console.error('Error fetching code:', error));
+})();
